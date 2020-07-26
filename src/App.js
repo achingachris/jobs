a44aca7e107310eef096074d065e4f1df6e268a0
ChrisAchinga <achinga.chris@gmail.com>
2020-07-26 16:13:35 +0300

    import useFetch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import useFetchJobs from './useFetchJobs'
+import { Container } from 'react-bootstrap'
 
 
 function App() {
-  return <h>Hello</h>
+
+  const { jobs, loading, error } = useFetchJobs()
+
+  return (
+    <Container>
+      {loading && <h1>Loading ...</h1>}
+      {error && <h1>Error, try refreshing</h1>}
+      <h1>{jobs.length}</h1>
+    </Container>
+  )
 }
 
 export default App;
