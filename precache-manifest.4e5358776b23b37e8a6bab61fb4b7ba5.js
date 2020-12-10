7b346518baba83e0f828cb10b7e57b89bdf86aad
ChrisAchinga <achinga.chris@gmail.com>
2020-12-10 13:44:38 +0300

    Updates

---
+++ precache-manifest.4e5358776b23b37e8a6bab61fb4b7ba5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b895f8d467c6b49522be6a64aaf2a98",
+    "url": "/jobs/index.html"
+  },
+  {
+    "revision": "f0e283b0a2d671d16957",
+    "url": "/jobs/static/css/main.4aa0e23b.chunk.css"
+  },
+  {
+    "revision": "d21a527e804465dd4ebd",
+    "url": "/jobs/static/js/2.5e86b373.chunk.js"
+  },
+  {
+    "revision": "b4ac570288b3bba4760d47e21228c95c",
+    "url": "/jobs/static/js/2.5e86b373.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0e283b0a2d671d16957",
+    "url": "/jobs/static/js/main.1a22e1f7.chunk.js"
+  },
+  {
+    "revision": "cd0b40a8a74b470cde53",
+    "url": "/jobs/static/js/runtime-main.84663fd0.js"
+  }
+]);
